1c8770390e7eb4657884e1cf201c984696854846
Yifu Ma <yifuma@yifus-mbp.dhcp.wustl.edu>
2016-02-29 10:24:52 -0600

    start to work on signup vc

---
+++ Ultrain/SignupVC.swift
@@ -0,0 +1,45 @@
+//
+//  SignupVC.swift
+//  Ultrain
+//
+//  Created by Yifu Ma on 2/28/16.
+//  Copyright © 2016 Yifu Ma. All rights reserved.
+//
+
+import UIKit
+
+class SignupVC: UIViewController {
+
+    @IBOutlet weak var NameField: UITextField!
+    @IBOutlet weak var UsernameField: UITextField!
+    @IBOutlet weak var PasswordField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func SignupTapped(sender: UIButton) {
+        let nameString = NameField.text! as NSString
+        let username = UsernameField.text! as NSString
+        let password = PasswordField.text! as NSString
+        
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
